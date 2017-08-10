ac0620f4146bcf729edbd2e7680e5f30470c5927
kiyoshi626 <travis.nakano@gmail.com>
2017-08-09 20:45:53 -0600

    updated field entry functionality

--- friends/friends/src/containers/friends_list.js
+++ friends/friends/src/containers/friends_list.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import { newFriends } from '../actions/new_friends';
+import { newFriends } from '../actions/new_friends';
 import { getFriends } from '../actions';
 // import axios from 'axios';
 
@@ -14,14 +14,23 @@ class Friendslist extends Component {
       <div>
         <form>
           <p>Name:</p><br />
-          <input type="text" name="firstname" value="First" />
+           <input type="text" name="firstname" value="First"
+              onChange={this.newFriends}
+              placeholder="Name..."
+              value={this.newFriends} />   
           <br />
           <p>Age:</p><br /> 
-          <input type="text" name="lastname" value="Age" />
+          <input type="text" name="Age" value="Age"
+            onChange={this.newFriends}
+            placeholder="Age..."
+            value={this.newFriends} />
           <p>Email:</p><br /> 
-          <input type="text" name="lastname" value="Email" />
+          <input type="text" name="Email" value="Email"
+            onChange={this.newFriends}
+            placeholder="E-Mail..."
+            value={this.newFriends} />
           <br /><br />
-          <input type="submit" value="Submit" /> 
+          <input type="submit" value="Submit" onSubmit={this.newFriends} /> 
         </form>
         <ul>
           {this.props.friends.map((friend, i) => {
@@ -40,6 +49,16 @@ class Friendslist extends Component {
   }
 }
 
+            // <div>
+            //      <form onSubmit={this.addTodo}> 
+            //         <input 
+            //             onChange={this.updateNewTodo}
+            //             placeholder="new todo..."
+            //             value={this.state.newTodo}
+            //         />
+            //     </form>
+            //     <List todos={this.state.todos} />
+            // </div>
 
 const mapStateToProps = (state) => {
     return {
