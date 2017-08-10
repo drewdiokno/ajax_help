b73276aed2b2c4be65a94f84c29ac4a69968e63e
Drew Diokno <drewdiokno@gmail.com>
2017-08-09 19:55:35 -0700

    update file name

--- friends/friends/src/App.js
+++ friends/friends/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Friendslist from './containers/Friends_list'
+import Friendslist from './containers/friends_list'
 
 class App extends Component {
   render() {
